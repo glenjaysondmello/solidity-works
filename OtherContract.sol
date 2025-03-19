d6369a418cb146aa27478e4acf677870f8d96751
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-03-20 00:00:00 +0530

    Create OtherContract.sol

---
+++ OtherContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+// Multiple smart contracts
+pragma solidity >=0.8.4;
+
+contract OtherSecond {
+    function doAgainSomething()
+    external 
+    pure 
+    returns (string memory){
+        return "This is other man";
+    }
+}
